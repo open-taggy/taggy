5dab958b429917dccfe71980f35a46e3fa93478e
Johannes Busching <b1tsteller@users.noreply.github.com>
2023-02-13 17:01:27 +0100

    removed unused files, created 0.1.0 release

--- taggy-cli/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import { taggy } from "@b1tsteller/taggy";
-const lib_1 = require("../../taggy/lib");
-lib_1.taggy.taggyCLI();
-lib_1.taggy.taggyVanilla("null");
-//# sourceMappingURL=index.js.map
--- taggy-cli/src/index.ts
+++
@@ -1,6 +0,0 @@
-// import { taggy } from "@b1tsteller/taggy";
-import { taggy } from "../../taggy/lib";
-
-taggy.taggyCLI();
-
-taggy.taggyVanilla("null");
